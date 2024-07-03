2736700d4c2eaf0a898b37fbe8c0882bccb6785f
tlsgptj <angele0709@naver.com>
2024-07-03 16:16:23 +0900

    WhoAmIActivity.kt 수정

--- app/src/main/java/campus/tech/kakao/contacts/WhoAmIActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/WhoAmIActivity.kt
@@ -20,12 +20,13 @@ class WhoAmIActivity : AppCompatActivity() {
 
         val contact = intent.getSerializableExtra("contact") as? Contact
         if (contact != null) {
-            findViewById<TextView>(R.id.nameTextView).text = contact.toString()
-            findViewById<TextView>(R.id.phoneTextView).text = contact.toString()
-            findViewById<TextView>(R.id.emailTextView).text = contact.toString()
-            findViewById<TextView>(R.id.BirTextView).text = contact.toString()
-            findViewById<TextView>(R.id.memoTextView).text = contact.toString()
-            findViewById<TextView>(R.id.genderTextView).text = contact.toString()
+            findViewById<TextView>(R.id.nameTextView).text = contact.name
+            findViewById<TextView>(R.id.phoneTextView).text = contact.phone
+            findViewById<TextView>(R.id.emailTextView).text = contact.email
+            findViewById<TextView>(R.id.BirTextView).text = contact.birthday
+            findViewById<TextView>(R.id.memoTextView).text = contact.message
+            findViewById<TextView>(R.id.genderTextView).text = contact.gender
+
         }
 
 
